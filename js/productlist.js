8bbf9bd875c6d93b7a5ed313423fdc3b5a48399a
JosefineStubner <josefine.stubner@gmail.com>
2022-02-17 16:21:34 +0100

    added function tå draw product(cards) based on category. Filter products depending on category.

--- js/productlist.js
+++ js/productlist.js
@@ -15,3 +15,45 @@ const queryString = new URLSearchParams(location.search);
 //toLowerCase för att komma runt eventuella stavfel.
 let qsCategory = queryString.get("category").toLowerCase();
 console.log(qsCategory);
+
+//hämta alla json-produkter / filtrera bort produkter utifrån category
+fetchJson().then((data) => {
+  const products = data.products.filter(product => product.category.toLowerCase() == qsCategory);
+
+  // för varje produkt skapa ett produktkort
+  // .forEach
+  products.forEach(function (product) {
+
+    // Varje produkt skall ha: Namn / Pris / Bild
+    //Skapa html-element.
+    const productCard = document.createElement("div");
+    productCard.classList.add("productCard");
+
+    const productParagraph = document.createElement("p");
+    productParagraph.innerText = product.name;
+
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+
+    //href på image fungerar inte, löser med a-tagg senare.
+    productImage.href = `Produktsida.html?name=${product.name}`;
+
+    //köpknapp
+    const buyBtn = document.createElement("button");
+
+    buyBtn.innerText = "Köp";
+
+    //Append elements to productCard
+    productCard.appendChild(productParagraph);
+    productCard.appendChild(productImage);
+    productCard.appendChild(buyBtn);
+
+    //append productCard to section
+    productList.appendChild(productCard);
+  });
+});
+
+// Funktion för att lägga till produkten i varukorgen
+
+
+// Textfält där användaren kan “söka” efter en produkt, alltså ett filter där innehållet i sökfältet skall matchas mot produktens namn och beskrivning (VG)
