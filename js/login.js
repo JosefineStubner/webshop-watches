2d562d7621cc65820a4f9fbdd95475dddd151f66
inspectah14 <lambada_41@hotmail.com>
2022-02-23 11:17:53 +0100

    fixed login status on navbar

--- js/login.js
+++ js/login.js
@@ -3,9 +3,6 @@ const loginForm = document.querySelector("#loginForm");
 const loginEmail = document.querySelector("#loginEmail");
 const loginPassword = document.querySelector("#loginPassword");
 
-const loginMenuLink = document.querySelector("#loginMenuLink");
-const loginMenuList = document.querySelector("#loginMenuList");
-
 const loggedInUser = () => {
   localStorage.removeItem("loggedInUser")
   localStorage.setItem("loggedInUser", JSON.stringify({
@@ -18,20 +15,13 @@ const loggedInUser = () => {
 
 let users = JSON.parse(localStorage.users);
 
-//if/else med nästlad if-sats.
 const checkUserInfo = () => {
-  //går igenom alla användare:
   users.forEach((user) => {
-  //för den användare som matchar angiven mail:
     if (user.email === loginEmail.value) {
-  //jämför endast lösenordet för denna användare:
       if (user.password === loginPassword.value) {
-  //ger denna alert om rätt:
         alert("login successful");
         loggedInUser();
       } else {
-  //ger denna alert om fel:
-  //OBS för närvarande kommer ingen alert, ska felsöka.
         alert("login failed");
       }
     }
